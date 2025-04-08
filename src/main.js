03c3cfa963d4e8dc89d802c4964f30ff16e9e01d
Olga <olgatravina@MacBook-Pro-Olga.local>
2025-04-08 16:19:12 +0300

    unit 2 styles

--- src/main.js
+++ src/main.js
@@ -1,2 +1,27 @@
 import 'normalize.css';
 import './style.scss';
+
+import { Navigation, Thumbs } from 'swiper/modules';
+import  Swiper from 'swiper'
+import 'swiper/css';
+
+
+ 
+ 
+ const thumbnailsSlider = new Swiper(".slider-thumbnails", {
+   spaceBetween: 10,
+   slidesPerView: 4,
+   freeMode: true,
+   watchSlidesProgress: true,
+ });
+ const productSwiper = new Swiper(".product__slider", {
+   spaceBetween: 10,
+   navigation: {
+     nextEl: ".product__slider-arrow_next",
+     prevEl: ".product__slider-arrow_prev",
+   },
+   modules: [Navigation, Thumbs],
+   thumbs: {
+     swiper: thumbnailsSlider,
+   },
+ });

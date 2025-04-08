6d3d78ec1879766b427a431c2c805d9b6da5b846
Olga <olgatravina@MacBook-Pro-Olga.local>
2025-04-09 00:07:11 +0300

    the second unit add style

--- src/main.js
+++ src/main.js
@@ -1,27 +1,24 @@
 import 'normalize.css';
 import './style.scss';
 
-import { Navigation, Thumbs } from 'swiper/modules';
-import  Swiper from 'swiper'
-import 'swiper/css';
+import { Navigation, Thumbs} from 'swiper/modules';
+import Swiper from 'swiper';
+import 'swiper/css'
 
-
- 
- 
- const thumbnailsSlider = new Swiper(".slider-thumbnails", {
-   spaceBetween: 10,
-   slidesPerView: 4,
-   freeMode: true,
-   watchSlidesProgress: true,
- });
- const productSwiper = new Swiper(".product__slider", {
-   spaceBetween: 10,
-   navigation: {
-     nextEl: ".product__slider-arrow_next",
-     prevEl: ".product__slider-arrow_prev",
-   },
-   modules: [Navigation, Thumbs],
-   thumbs: {
-     swiper: thumbnailsSlider,
-   },
- });
+const thumbnailsSlider= new Swiper(".slider-thumbnails", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const productSwiper = new Swiper(".product__slider", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".product__slider-arrow-next",
+    prevEl: ".product__slider-arrow-prev",
+  },
+  modules: [Navigation, Thumbs],
+  thumbs: {
+    swiper: thumbnailsSlider,
+  },
+});
